f6326ff3e8f01a9c75b789428df3bcb106c7d45e
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2020-05-25 21:30:29 +0300

    Create HTTPGET_DATE.cs

---
+++ HTTPGET_DATE.cs
@@ -0,0 +1,45 @@
+        /// <summary>
+        /// Convert a UTC Date String of format yyyyMMddTHHmmZ into a Local Date
+        /// </summary>
+        /// <param name="dateString"></param>
+        /// <returns></returns>
+        public DateTime BuildDateTimeFromYAFormat(string dateString)
+        {
+            Regex r = new Regex(@"^\d{4}\d{2}\d{2}T\d{2}\d{2}\d{2}Z$");
+            if (!r.IsMatch(dateString))
+            {
+                throw new FormatException(
+                    string.Format("{0} is not the correct format. Should be yyyyMMddTHHmmssZ", dateString));
+            }
+
+            // in israel will add 2 hours forward +2
+            DateTime dt = DateTime.ParseExact(dateString, "yyyyMMddTHHmmssZ", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+
+            return dt;
+        }
+        
+        [HttpGet]
+        [Route("api/purchase/getfromdate")]
+        public IHttpActionResult StartFromDate([FromUri]string startDate)
+        {
+            try
+            {
+                // need to validate parameters before continue ...
+                if (bulkSize <= 0)
+                    return BadRequest("bulk size must be greater euqal than 1");
+
+                var startDateDT = BuildDateTimeFromYAFormat(startDate);
+
+                // ...
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex); // +also write to log file
+
+                // consider which error to send to customer
+                // choose between bad-request and internal-error
+                return BadRequest("Something went wrong ... check the parameter you send. please contact support");
+            }
+
+        }
+        
